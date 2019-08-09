4f2da08626964ae85312724f28f8b77a6e0c5865
Mati Dastugue <matias.dastugue@altoros.com>
2019-08-09 17:53:55 -0300

    get rates thru differents dEfi protocols

--- routes/rates.js
+++ routes/rates.js
@@ -3,14 +3,10 @@ const router = express.Router();
 const rateService = require('../services/RateService');
 
 router.get('/:token', async (req, res, next) => {
+  const protocol = (req.query.protocol).toLowerCase();
   try {
-    const response = await rateService.get(req.params.token);
-    const tokenRate = {
-      borrow_rate: (response.borrow_rate.value * 100).toFixed(2),
-      supply_rate: (response.supply_rate.value * 100).toFixed(2),
-      exchange_rate: (response.exchange_rate.value * 100).toFixed(2)
-    }
-    res.json({ token: req.params.token, rates: tokenRate });
+    const tokenRate = await rateService.get(req.params.token, protocol);
+    res.json({ protocol, token: req.params.token, rates: tokenRate });
   } catch (e) {
     next(e);
   }
--- services/RateService.js
+++ services/RateService.js
@@ -1,9 +1,40 @@
 var axios = require('axios');
 var utils = require('../utils');
 
-const get = async token => {
+const get = async (token, protocol) => {
+    let tokenRate = {};
+    switch (protocol) {
+        case 'compound':
+            tokenRate = await getCompoundRate(token);
+            break;
+        case 'dydx':
+            tokenRate = await getDyDxRate(token);
+            break;
+        default:
+            break;
+    }
+
+    return tokenRate;
+}
+
+const getCompoundRate = async token => {
     const tokenDetails = await axios.get(`https://api.compound.finance/api/v2/ctoken?addresses[]=${utils.rates[token].address}`);
-    return tokenDetails.data.cToken[0];
+    const response = tokenDetails.data.cToken[0];
+    return {
+        borrow_rate: (response.borrow_rate.value * 100).toFixed(2),
+        supply_rate: (response.supply_rate.value * 100).toFixed(2),
+        exchange_rate: (response.exchange_rate.value * 100).toFixed(2)
+    }
+
+}
+
+const getDyDxRate = async token => {
+    const tokenDetails = await axios.get("https://api.dydx.exchange/v1/markets");
+    const response = tokenDetails.data.markets.find(market => market.name === token);
+    return {
+        borrow_rate: (response.totalSupplyAPY * 100).toFixed(2),
+        supply_rate: (response.totalBorrowAPY * 100).toFixed(2)
+    }
 }
 
 module.exports = {
