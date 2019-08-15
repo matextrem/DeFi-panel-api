aee1a90cc8e7de54f767113b2310d87658a13093
Mati Dastugue <matias.dastugue@altoros.com>
2019-08-15 15:01:33 -0300

    add error handlers

--- app.js
+++ app.js
@@ -15,23 +15,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+
 app.use('/rates', ratesRouter);
 
-
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use('*', function (req, res, next) {
+  return next(createError(404, 'This url does not exist'));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  console.log(err);
+  res.status(err.status || 500).send({ error: err.message });
 });
 
 module.exports = app;
